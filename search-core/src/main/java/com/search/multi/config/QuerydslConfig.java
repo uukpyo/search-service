cf8aa361b7905fd71caa3b17c2aff4cceffb08bb
uukpyo <dnrvy6719@gmail.com>
2023-03-22 18:54:39 +0900

    create:search: querydsl 설정, h2 db 설정

---
+++ search-core/src/main/java/com/search/multi/config/QuerydslConfig.java
@@ -0,0 +1,22 @@
+package com.search.multi.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+}
